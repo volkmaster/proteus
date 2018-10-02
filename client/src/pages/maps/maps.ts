b34187c48428342c29353f286935f46410e046ba
Matevz Lenic <matevz.lenic@gmail.com>
2018-10-03 00:23:18 +0200

    Dashboard scrolling now enabled again - Maps sort of work - still a problem with options

--- client/src/pages/maps/maps.ts
+++ client/src/pages/maps/maps.ts
@@ -14,6 +14,12 @@ import { dediscina } from '../../assets/data/dediscina';
 })
 export class MapsPage {
 
+    private transportMap = {
+        1 : 'driving',
+        2 : 'walking',
+        3 : 'bicycling',
+        4 : 'transit',
+    }
     constructor(
         private navCtrl: NavController,
         private launchNavigator: LaunchNavigator,
@@ -21,10 +27,18 @@ export class MapsPage {
     ) { }
 
     ionViewDidLoad() {
-        this.launchGoogleMaps();
+        //this.launchGoogleMaps();
     }
 
-    private launchGoogleMaps() {
+    /**
+     *
+     * @param transportMode integer that represents different options for transport mode
+     * 1 - Driving
+     * 2 - Walking
+     * 3 - Bicycling
+     * 4 - Transit
+     */
+    private launchGoogleMaps(transportMode: number) {
         const indices = [1, 2, 3, 4, 5];
 
         const options: LaunchNavigatorOptions = { app: this.launchNavigator.APP.GOOGLE_MAPS };
@@ -35,10 +49,35 @@ export class MapsPage {
         }
         destination += this.formatCoordinates(indices[indices.length - 1]);
 
-        this.launchNavigator.navigate(destination, options).then(
-            success => this.toastService.show('Google Maps works.'),
-            error => this.toastService.show('Google Maps does not work')
-        );
+        this.launchNavigator.isAppAvailable(this.launchNavigator.APP.GOOGLE_MAPS).then((isAvailable) => {
+            var app;
+
+            var transMode = this.transportMap[transportMode];
+            transMode = transMode == null ? this.transportMap[1] : transMode;
+
+            let options: LaunchNavigatorOptions = {
+                app: app,
+                transportMode: transMode
+              };
+
+            if(isAvailable) {
+                app = this.launchNavigator.APP.GOOGLE_MAPS;
+
+                this.launchNavigator.navigate(destination)
+                .then(() => {this.onMapsSuccess();})
+                .catch(err => {this.onMapsError(err);});
+            }
+
+
+        });
+    }
+
+    private onMapsError(error: string) {
+        this.toastService.show("Error launching maps " + error, 5000);
+    }
+
+    private onMapsSuccess() {
+        this.toastService.show("Maps successfully launched.", 5000);
     }
 
     private formatCoordinates(index: number): string {
