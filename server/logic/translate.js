4bc3b055d06bca18fcf19bf0f38f1ee6150a0eb8
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-10-03 16:48:56 +0200

    Add missing translate files and package json for google translate api

---
+++ server/logic/translate.js
@@ -0,0 +1,14 @@
+import { Translate } from '@google-cloud/translate'
+
+const projectId = 'project-id-5593195159403283543'
+
+const translate = new Translate({
+    projectId: projectId,
+})
+
+async function sloveneToEnglish(text) {
+    let results = await translate.translate(text, 'en')
+    return results[0]
+}
+
+module.exports = { sloveneToEnglish }
