d30112ee88b1a48caafad7df19ab811ff9cb0531
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-10-03 10:49:51 +0200

    Remove commented code from user controller in route controller

--- server/controllers/route.js
+++ server/controllers/route.js
@@ -2,21 +2,6 @@ import express from 'express'
 const router = express.Router()
 
 import routeLogic from '../logic/route'
-// import { sendError } from '../utils/error-utils'
-
-// router.get('/:id', async (req, res, next) => {
-//     const userId = req.params.id
-//     if (!userId) {
-//         res.status(400).send('User ID is required.')
-//     }
-
-//     try {
-//         const user = await userLogic.get(userId)
-//         res.json(user)
-//     } catch (error) {
-//         sendError(res, error)
-//     }
-// })
 
 router.get('', (req, res, next) => {
     const params = {
