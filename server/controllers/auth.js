43edfb4affb83ada098db2e89cd732b4e6063f16
Matevz Lenic <matevz.lenic@gmail.com>
2018-10-07 11:30:22 +0200

    Changed bcrypt to bcryptjs

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 import userLogic from '../logic/user'
 import { verifyToken } from '../middleware/middleware'
